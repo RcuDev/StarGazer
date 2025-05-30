3f10d9996e51082befca303d303c7117387afc53
Raul Corvo <raul.corvo@mango.com>
2025-05-30 19:44:53 +0200

    Update libraries

--- ds/build.gradle.kts
+++ ds/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -11,7 +10,6 @@ plugins {
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
